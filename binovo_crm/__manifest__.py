835c513f6b2a5243fe598ba1d348871a75aedc75
Allan Aguilar <allanaguilar.a@gmail.com>
2025-09-07 12:26:24 +0200

    [ADD] 3 binovo_crm se agrega campo a formulario "Contact Us" de la web

--- binovo_crm/__manifest__.py
+++ binovo_crm/__manifest__.py
@@ -7,7 +7,7 @@
     ''',
     'author': 'Allan Aguiilar',
     'website': 'https://binovo.es/',
-    'version': '2025.09.03',
+    'version': '2025.09.09',
     'license': 'Other proprietary',
     'maintainer': '',
     'contributors': [],
@@ -16,12 +16,16 @@
     'sequence': 1050,
     'depends': [
         'crm',
+        'website',
     ],
     'data': [               
+        # D A T A
+        'data/ir_model_fields.xml',
         
         # V I E W S
         'views/crm_lead_views.xml',
         'views/res_partner_views.xml',
+        'views/website_templates_contactus.xml',
 
     ],
     'demo': [
